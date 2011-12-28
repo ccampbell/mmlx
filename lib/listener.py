9f96850a0f9ee937ea2c18439621a63c3cdea90a
Craig Campbell <iamcraigcampbell@gmail.com>
2011-12-28 17:02:44 -0500

    Support listening for changes to a single file as well as processing a file or directory once

--- lib/listener.py
+++ lib/listener.py
@@ -18,33 +18,41 @@ class Listener(object):
     def onChange(self, callback):
         self.callback = callback
 
+    def process(self, start, end, is_dir = None):
+        if is_dir is None:
+            is_dir = os.path.isdir(start)
+
+        if is_dir:
+            files = self.getFilesFromDir(start, "xmml")
+            output_file = None
+        else:
+            files = [start]
+            output_file = end
+
+        initial_output_file = output_file
+
+        for file in files:
+            output_file = initial_output_file
+
+            filename = os.path.basename(file)
+            if filename.startswith('_'):
+                continue
+
+            last_changed = os.stat(file).st_mtime
+            output_file = output_file if output_file is not None else os.path.join(end, filename.replace(".xmml", ".mml"))
+
+            if not self.file_list.has_key(file):
+                self.file_list[file] = last_changed
+                self.callback(file, output_file)
+                continue
+
+            if last_changed != self.file_list[file]:
+                self.logger.log("detected change to: " + file)
+                self.file_list[file] = last_changed
+                self.callback(file, output_file)
+
     def watch(self, start, end):
         is_dir = os.path.isdir(start)
         while 1:
-            if is_dir:
-                files = self.getFilesFromDir(start, "xmml")
-                output_file = None
-            else:
-                files = [start]
-                output_file = end
-
-            for file in files:
-                filename = os.path.basename(file)
-
-                if filename.startswith('_'):
-                    continue
-
-                last_changed = os.stat(file).st_mtime
-                output_file = output_file if output_file is not None else os.path.join(end, filename.replace(".xmml", ".mml"))
-
-                if not self.file_list.has_key(file):
-                    self.file_list[file] = last_changed
-                    self.callback(file, output_file)
-                    continue
-
-                if last_changed != self.file_list[file]:
-                    self.logger.log("detected change to: " + file)
-                    self.file_list[file] = last_changed
-                    self.callback(file, output_file)
-
+            self.process(start, end, is_dir)
             time.sleep(.5)

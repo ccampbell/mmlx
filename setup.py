6642c5cf422bf1edcfdc295c09e645d2addf0152
Craig Campbell <iamcraigcampbell@gmail.com>
2012-01-04 00:59:45 -0500

    Start using setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='xmml',
+    version='1.0',
+    description='Extended version of music macro language which generates MML and NSF files for Nintendo music composing',
+    author='Craig Campbell',
+    author_email='iamcraigcampbell@gmail.com',
+    packages=['xmml'],
+    scripts=['xmml']
+)
